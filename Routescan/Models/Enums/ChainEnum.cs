64737a647b07a55e1e70bced072110d237a2c96c
Mehmet Emin KARADAĞ <emin.karadag@ogr.sakarya.edu.tr>
2024-04-07 10:51:07 +0300

    Desteklenen zincir sayısı 32'ye çıkarıldı.

--- Routescan/Models/Enums/ChainEnum.cs
+++ Routescan/Models/Enums/ChainEnum.cs
@@ -7,25 +7,103 @@ namespace Routescan.Models.Enums
         [Display(Name = "43114")]
         C_CHAIN,
 
-        [Display(Name = "1")]
-        ETHEREUM,
-
-        [Display(Name = "324")]
-        ZKSYNC_ERA,
-
-        [Display(Name = "10")]
-        OPTIMISM,
+        [Display(Name = "53935")]
+        DFK,
 
         [Display(Name = "432204")]
         DEXALOT,
 
+        [Display(Name = "8888")]
+        XANA_CHAIN,
+
+        [Display(Name = "1234")]
+        STEP_NETWORK,
+
+        [Display(Name = "10507")]
+        NUMBERS,
+
+        [Display(Name = "7979")]
+        DOS,
+
+        [Display(Name = "2044")]
+        SHRAPNEL,
+
+        [Display(Name = "333000333")]
+        MELD,
+
+        [Display(Name = "262018")]
+        LOCO_LEGENDS,
+
+        [Display(Name = "6119")]
+        UPTN,
+
+        [Display(Name = "3011")]
+        PLAYA3ULL_GAMES,
+
+        [Display(Name = "1088")]
+        METIS,
+
+        [Display(Name = "324")]
+        ZKSYNC_ERA,
+
+        [Display(Name = "1")]
+        ETHEREUM,
+
+        [Display(Name = "5000")]
+        MANTLE,
+
+        [Display(Name = "288")]
+        BOBA_ETHEREUM,
+
+        [Display(Name = "14")]
+        FLARE_MAINNET,
+
         [Display(Name = "8453")]
         BASE,
+
+        [Display(Name = "19")]
+        SONGBIRD_CANARY,
+
+        [Display(Name = "10")]
+        OPTIMISM,
+
+        [Display(Name = "56288")]
+        BOBA_BNB,
 
         [Display(Name = "88888")]
         CHILIZ,
 
-        [Display(Name = "1088")]
-        METIS
+        [Display(Name = "424")]
+        PGN,
+
+        [Display(Name = "7777777")]
+        ZORA,
+
+        [Display(Name = "1080")]
+        MINTARA,
+
+        [Display(Name = "4337")]
+        BEAM,
+
+        [Display(Name = "1992")]
+        HUBBLE_EXCHANGE,
+
+        [Display(Name = "291")]
+        ORDERLY,
+
+        [Display(Name = "34443")]
+        MODE,
+
+        [Display(Name = "81457")]
+        BLAST,
+
+        [Display(Name = "62707")]
+        TELEPORTER_1,
+
+        [Display(Name = "70953")]
+        TELEPORTER_2,
+
+        [Display(Name = "7887")]
+        KINTO
     }
 }
